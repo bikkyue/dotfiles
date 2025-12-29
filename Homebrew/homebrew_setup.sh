ea707c9aeb7085da8347dabc0573c6a3a385c137
K Bikkyue <biki2906.service@gmail.com>
2025-12-29 18:46:18 +0900

    zsh切り替え

--- Homebrew/homebrew_setup.sh
+++ Homebrew/homebrew_setup.sh
@@ -10,3 +10,27 @@ fi
 # インストール成功後（または既にインストール済みの場合）に環境を読み込む
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 brew bundle install --file=Brewfile
+
+# ---Homebrewでインストールしたzshをデフォルトシェルに切り替える
+
+# Brewでzshがインストールされているか確認
+if ! brew list zsh &>/dev/null; then
+    echo "zshがインストールされていないため、インストールを開始します..."
+    brew install zsh
+else
+    echo "zshはすでにインストールされています。"
+fi
+
+# インストールしたzshのパスを取得
+ZSH_PATH=$(brew --prefix)/bin/zsh
+
+# インストールされたzshがシェルとして許可されているか確認し、追加
+if ! grep -Fxq "$ZSH_PATH" /etc/shells; then
+    echo "$ZSH_PATH" | sudo tee -a /etc/shells
+fi
+
+# デフォルトシェルをインストールしたzshに変更
+chsh -s "$ZSH_PATH"
+
+# 設定の反映を促すメッセージ
+echo "brewでインストールしたzshをデフォルトシェルに設定しました。ターミナルを再起動してください。"
