a218bb8fb99f75f01878b58569f088abd1d3f40c
K Bikkyue <biki2906.service@gmail.com>
2025-12-31 17:50:43 +0900

    あああ

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -1,28 +1,26 @@
 return {
   {
-    "https://github.com/nvim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     branch = "main",
     config = function()
-      -- パーサーのインストール先を指定
-      require'nvim-treesitter'.setup {
-        -- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
-        install_dir = vim.fn.stdpath('data') .. '/site'
-      }
-      require'nvim-treesitter'.install { 'lua' }
+      require("nvim-treesitter").install({
+        "lua",
+        "go",
+        "markdown",
+        "astro",
+        "python",
+        "bash",
+      })
 
-      -- 自動ハイライトの有効化
-      vim.api.nvim_create_autocmd('FileType', {
-      pattern = { 'lua' },
-      callback = function() 
-        vim.treesitter.start() 
-        vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-      
-      end,
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function(args)
+          pcall(vim.treesitter.start, args.buf)
+          vim.wo[args.buf].foldmethod = "expr"
+          vim.wo[args.buf].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+        end,
       })
 
       vim.treesitter.language.register("bash", { "sh", "zsh" })
-      
-    end
-  }
+    end,
+  },
 }
