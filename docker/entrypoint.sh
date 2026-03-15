f199d7fbf281eb15df44113cef35817894b301d5
K Bikkyue <biki2906.service@gmail.com>
2026-03-15 15:51:01 +0900

    chore: 不要な処理削除

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,8 +7,6 @@ if [ -d "$HOME/.ssh-host" ]; then
     sudo chown -R "$USER":"$USER" "$HOME/.ssh"
     chmod 700 "$HOME/.ssh"
     find "$HOME/.ssh" -type f -exec chmod 600 {} \;
-    find "$HOME/.ssh" -type f -name "*.pub" -exec chmod 644 {} \;
-    find "$HOME/.ssh" -type f -name "known_hosts" -exec chmod 644 {} \;
 fi
 
 exec "$@"
