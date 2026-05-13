6ea74449d10ba9976dab1c7519ab52bddf5657d7
bikkyue <121682296+bikkyue@users.noreply.github.com>
2026-05-13 23:39:05 +0900

    add: nvim-tree

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,3 +2,6 @@ vim.keymap.set('n', '<leader>ff', function() require('telescope.builtin').find_f
 vim.keymap.set('n', '<leader>fg', function() require('telescope.builtin').live_grep() end, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', function() require('telescope.builtin').buffers() end, { desc = 'Telescope buffers' })
 vim.keymap.set('n', '<leader>fh', function() require('telescope.builtin').help_tags() end, { desc = 'Telescope help tags' })
+
+-- nvim-tree
+vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
