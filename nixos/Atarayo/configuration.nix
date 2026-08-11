53b9202f5a5203217a1ebc50449e0403f72110ca
bikkyue <121682296+bikkyue@users.noreply.github.com>
2026-08-11 12:55:08 +0900

    feat: fcitx5の設定をNix管理に変更

--- nixos/Atarayo/configuration.nix
+++ nixos/Atarayo/configuration.nix
@@ -51,7 +51,35 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+    fcitx5 = {
+      waylandFrontend = true;
+      addons = with pkgs; [ fcitx5-mozc ];
+      settings = {
+        globalOptions = {
+          "Hotkey/TriggerKeys" = {
+            "0" = "Menu";
+            "1" = "Zenkaku_Hankaku";
+          };
+          "Hotkey/AltTriggerKeys"."0" = "Shift+Shift_R";
+        };
+        inputMethod = {
+          "Groups/0" = {
+            Name = "Default";
+            "Default Layout" = "us";
+            DefaultIM = "mozc";
+          };
+          "Groups/0/Items/0" = {
+            Name = "keyboard-us";
+            Layout = "";
+          };
+          "Groups/0/Items/1" = {
+            Name = "mozc";
+            Layout = "";
+          };
+          "GroupOrder"."0" = "Default";
+        };
+      };
+    };
   };
 
   environment.etc."niri/config.kdl".text = ''
